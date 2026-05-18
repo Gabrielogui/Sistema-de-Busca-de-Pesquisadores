ee79bf954596d11ce4f890abb377ca879ff7bf22
VerttB <alyssonoliveira456@gmail.com>
2026-05-18 14:41:34 -0300

    feat: config and prisma service as global module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+import { GruposPesquisaModule } from './grupos-pesquisa/grupos-pesquisa.module';
+import { PesquisadoresModule } from './pesquisadores/pesquisadores.module';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    isGlobal: true,
+    envFilePath: '.env',
+  }),{
+    module: PrismaModule,
+    global: true,
+  }, GruposPesquisaModule, PesquisadoresModule],
   controllers: [AppController],
   providers: [AppService],
 })
