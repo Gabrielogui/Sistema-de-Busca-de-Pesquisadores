8f15ce6113bde449d3a4ea895a88ef370a901dbe
Gabrielogui <gabrielcsro@gmail.com>
2026-06-03 10:45:51 -0300

    feat: Colocando componentes de shadcn(Botao) + Utiliando no card + Imagem no card com detalhes ao lado

--- apps/web/src/components/researcher/ResearcherCardDetails.tsx
+++ apps/web/src/components/researcher/ResearcherCardDetails.tsx
@@ -1,12 +1,16 @@
-import { GraduationCap } from "lucide-react";
+"use client";
+
+import { GraduationCap, MapPin } from "lucide-react";
 import Image from "next/image";
+import { Button } from "../ui/button";
 
 export default function ResearcherCardDetails() {
     return (
-        <div className="flex gap-4 border rounded-md w-full h-75">
+        <div className="flex gap-4 border border-slate-300 rounded-md w-full h-75">
             {/* IMAGEM DO PESQUISADOR */}
-            <div>
-                <Image src="https://picsum.photos/200/300" width={200} height={300} alt="Imagem do pesquisador"></Image>
+            <div className="relative w-70">
+                <Image src="https://picsum.photos/200/300" fill alt="Imagem do pesquisador"
+                    className="rounded-l-md object-cover object-center"></Image>
             </div>
 
             {/* DADOS DO PESQUISADOR */}
@@ -14,15 +18,19 @@ export default function ResearcherCardDetails() {
                 <div className="flex flex-col">
                     <div className="flex justify-between items-center">
                         <span className="text-lg">Nome do pesquisador</span>
-                        <span className="p-2 bg-gray-200 rounded-md">Computação</span>
+                        <span className="p-2 bg-gray-100 rounded-full">Computação</span>
                     </div>
                     <div className="flex items-center text-sm gap-2 text-gray-500 -mt-2">
                         <GraduationCap />
                         <span className="">Mestrado</span>
                     </div>
+                    <div className="flex items-center text-sm gap-2 text-gray-500">
+                        <MapPin />
+                        <span className="">Universidade do estado da Bahia</span>
+                    </div>
                 </div>
                 
-                <div className="p-3 bg-gray-200 rounded-md overflow-auto">
+                <div className="p-3 bg-gray-100 rounded-md overflow-auto">
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores obcaecati aliquid saepe delectus impedit? Voluptatum qui fugiat voluptatem itaque quisquam, earum placeat magni perspiciatis beatae similique, aut, facilis libero hic?
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores obcaecati aliquid saepe delectus impedit? Voluptatum qui fugiat voluptatem itaque quisquam, earum placeat magni perspiciatis beatae similique, aut, facilis libero hic?
                     Lorem ipsum dolor sit, amet consectetur adipisicing elit. Asperiores obcaecati aliquid saepe delectus impedit? Voluptatum qui fugiat voluptatem itaque quisquam, earum placeat magni perspiciatis beatae similique, aut, facilis libero hic?
@@ -34,7 +42,7 @@ export default function ResearcherCardDetails() {
                 </div>
                 
                 <div className="flex gap-2 border-t p-2 justify-end w-full">
-                    <span>Visualizar mais</span>
+                    <Button variant="ghost">Visualizar mais</Button>
                 </div>
 
             </div>
